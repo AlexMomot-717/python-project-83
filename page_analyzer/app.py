330f34d40258dfb91bfdb29b76ba0b80c04ff3b4
alexandra <momotav717@gmail.ru>
2024-02-02 20:49:27 +0600

    feat(step_4): add urls checks

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -4,7 +4,9 @@ from typing import Any
 from dotenv import load_dotenv
 from flask import Flask, flash, redirect, render_template, request, url_for
 from page_analyzer.utils.db import (
+    create_check,
     create_url,
+    get_checks_data,
     get_url_by_name,
     get_url_context,
     get_urls_data,
@@ -34,18 +36,19 @@ def add_url() -> Any:
     url_id = get_url_by_name(url_name)
     if url_id:
         flash("Страница уже существует", "info")
-        return redirect(url_for("show_url", id=url_id))
+        return redirect(url_for("show_url", url_id=url_id))
     url_id = create_url(url_name)
     flash("Страница успешно дoбавлена", "success")
-    return redirect(url_for("show_url", id=url_id))
+    return redirect(url_for("show_url", url_id=url_id))
 
 
-@app.route("/urls/<int:id>")
-def show_url(id: int) -> str:
-    url_data = get_url_context(id)
+@app.route("/urls/<int:url_id>")
+def show_url(url_id: int) -> str:
+    url_data = get_url_context(url_id)
     if not url_data:
         return render_template("page_not_found.html")
-    return render_template("url.html", url=url_data)
+    checks = get_checks_data(url_id)
+    return render_template("url.html", url=url_data, checks=checks)
 
 
 @app.route("/urls")
@@ -55,3 +58,9 @@ def list_urls() -> str:
         "urls.html",
         urls=urls_data_list,
     )
+
+
+@app.route("/urls/<int:url_id>/checks", methods=["POST"])
+def add_url_check(url_id: int) -> Any:
+    create_check(url_id)
+    return redirect(url_for("show_url", url_id=url_id))
--- page_analyzer/utils/db.py
+++ page_analyzer/utils/db.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any, List
+from typing import Any, Dict, List
 
 import psycopg2
 import psycopg2.extras
@@ -13,7 +13,7 @@ def connect_db() -> Any:
     return conn
 
 
-def get_url_context(url_id: int) -> dict[str, int | str] | None:
+def get_url_context(url_id: int) -> Dict[str, int | str] | None:
     conn = connect_db()
     with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as dict_cur:
         dict_cur.execute(
@@ -22,17 +22,12 @@ def get_url_context(url_id: int) -> dict[str, int | str] | None:
                 "int": url_id,
             },
         )
-        target_url_data = dict_cur.fetchone()
-        if target_url_data:
+        target_url_data: dict[str, int | str] | None = dict_cur.fetchone()
+        if not target_url_data:
             conn.close()
-            actual_url_data: dict[str, int | str] = {
-                "id": url_id,
-                "name": str(target_url_data["name"]),
-                "created_at": str(target_url_data["created_at"]),
-            }
-            return actual_url_data
+            return None
     conn.close()
-    return None
+    return target_url_data
 
 
 def get_url_by_name(url_name: str) -> int | None:
@@ -68,10 +63,75 @@ def create_url(url_name: str) -> int:
     return int(url_id)
 
 
-def get_urls_data() -> List[int | str]:
+def get_urls_data() -> List[Dict[str, Any]]:
+    conn = connect_db()
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as dict_cur:
+        dict_cur.execute(
+            "SELECT DISTINCT ON (u.id)"
+            " u.id AS url_id,"
+            " u.name AS url_name,"
+            " COALESCE(c.created_at::varchar, '') AS created_at,"
+            " COALESCE(c.status_code::varchar, '') AS status_code"
+            " FROM urls u"
+            " LEFT JOIN checks c"
+            " ON c.url_id = u.id"
+            " ORDER BY u.id DESC, c.created_at DESC;"
+        )
+        urls_data_list = dict_cur.fetchall()
+    conn.close()
+    actual_urls_data = []
+    for url_data in urls_data_list:
+        actual_urls_data.append(url_data)
+    return actual_urls_data
+
+
+def generate_check_context() -> Dict[str, int | str]:
+    status_code = 200
+    h1 = ""
+    title = ""
+    description = ""
+    check_context: dict[str, int | str] = {
+        "response_code": status_code,
+        "h1": h1,
+        "title": title,
+        "description": description,
+    }
+    return check_context
+
+
+def create_check(url_id: int) -> None:
+    check_context = generate_check_context()
     conn = connect_db()
     with conn.cursor() as cur:
-        cur.execute("SELECT * FROM urls ORDER BY id DESC;")
-        urls_data_list = cur.fetchall()
+        cur.execute(
+            "INSERT INTO checks"
+            " (url_id, status_code, h1, title, description)"
+            " VALUES (%s, %s, %s, %s, %s);",
+            (
+                url_id,
+                check_context["response_code"],
+                check_context["h1"],
+                check_context["title"],
+                check_context["description"],
+            ),
+        )
+        conn.commit()
     conn.close()
-    return list(urls_data_list)
+    return None
+
+
+def get_checks_data(url_id: int) -> List[Dict[str, int | str]]:
+    conn = connect_db()
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as dict_cur:
+        dict_cur.execute(
+            "SELECT * FROM checks WHERE url_id=%(int)s ORDER BY id DESC;",
+            {
+                "int": url_id,
+            },
+        )
+        url_checks = dict_cur.fetchall()
+    conn.close()
+    actual_checks_data = []
+    for check_data in url_checks:
+        actual_checks_data.append(dict(check_data))
+    return actual_checks_data
