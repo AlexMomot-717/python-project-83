393c957c256193de5d8844c606a056205a21bc51
alexandra <momotav717@gmail.ru>
2024-02-02 20:49:27 +0600

    feat(step_4): add urls checks

--- database.sql
+++ database.sql
@@ -4,3 +4,14 @@ CREATE TABLE urls (
     name varchar(255) UNIQUE NOT NULL,
     created_at DATE DEFAULT NOW() NOT NULL
 );
+
+DROP TABLE IF EXISTS checks CASCADE;
+CREATE TABLE checks (
+    id serial PRIMARY KEY,
+    url_id bigint REFERENCES urls (id),
+    status_code integer,
+    h1 text,
+    title text,
+    description text,
+    created_at DATE DEFAULT NOW() NOT NULL
+);
